ad836b7fea8c1317fc2df986e9384c2df16fe8ff
TheGimliGloinson <35769341+TheGimliGloinson@users.noreply.github.com>
2018-01-26 08:52:42 +0100

    Blabla
    Memeory

---
+++ Kul_læringsapp/Kul_læringsapp/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Kul_læringsapp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            this.pb2 = new System.Windows.Forms.PictureBox();
+            this.pb3 = new System.Windows.Forms.PictureBox();
+            this.pb6 = new System.Windows.Forms.PictureBox();
+            this.pb5 = new System.Windows.Forms.PictureBox();
+            this.pb4 = new System.Windows.Forms.PictureBox();
+            this.pb9 = new System.Windows.Forms.PictureBox();
+            this.pb8 = new System.Windows.Forms.PictureBox();
+            this.pb7 = new System.Windows.Forms.PictureBox();
+            this.btStart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb7)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pb1
+            // 
+            this.pb1.Location = new System.Drawing.Point(246, 116);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(135, 131);
+            this.pb1.TabIndex = 0;
+            this.pb1.TabStop = false;
+            this.pb1.Click += new System.EventHandler(this.pb1_Click);
+            // 
+            // pb2
+            // 
+            this.pb2.Location = new System.Drawing.Point(398, 116);
+            this.pb2.Name = "pb2";
+            this.pb2.Size = new System.Drawing.Size(135, 131);
+            this.pb2.TabIndex = 1;
+            this.pb2.TabStop = false;
+            this.pb2.Click += new System.EventHandler(this.pb1_Click);
+            // 
+            // pb3
+            // 
+            this.pb3.Location = new System.Drawing.Point(551, 116);
+            this.pb3.Name = "pb3";
+            this.pb3.Size = new System.Drawing.Size(135, 131);
+            this.pb3.TabIndex = 2;
+            this.pb3.TabStop = false;
+            this.pb3.Click += new System.EventHandler(this.pb1_Click);
+            // 
+            // pb6
+            // 
+            this.pb6.Location = new System.Drawing.Point(551, 265);
+            this.pb6.Name = "pb6";
+            this.pb6.Size = new System.Drawing.Size(135, 131);
+            this.pb6.TabIndex = 5;
+            this.pb6.TabStop = false;
+            this.pb6.Click += new System.EventHandler(this.pb1_Click);
+            // 
+            // pb5
+            // 
+            this.pb5.Location = new System.Drawing.Point(398, 265);
+            this.pb5.Name = "pb5";
+            this.pb5.Size = new System.Drawing.Size(135, 131);
+            this.pb5.TabIndex = 4;
+            this.pb5.TabStop = false;
+            this.pb5.Click += new System.EventHandler(this.pb1_Click);
+            // 
+            // pb4
+            // 
+            this.pb4.Location = new System.Drawing.Point(246, 265);
+            this.pb4.Name = "pb4";
+            this.pb4.Size = new System.Drawing.Size(135, 131);
+            this.pb4.TabIndex = 3;
+            this.pb4.TabStop = false;
+            this.pb4.Click += new System.EventHandler(this.pb1_Click);
+            // 
+            // pb9
+            // 
+            this.pb9.Location = new System.Drawing.Point(551, 417);
+            this.pb9.Name = "pb9";
+            this.pb9.Size = new System.Drawing.Size(135, 131);
+            this.pb9.TabIndex = 8;
+            this.pb9.TabStop = false;
+            this.pb9.Click += new System.EventHandler(this.pb1_Click);
+            // 
+            // pb8
+            // 
+            this.pb8.Location = new System.Drawing.Point(398, 417);
+            this.pb8.Name = "pb8";
+            this.pb8.Size = new System.Drawing.Size(135, 131);
+            this.pb8.TabIndex = 7;
+            this.pb8.TabStop = false;
+            this.pb8.Click += new System.EventHandler(this.pb1_Click);
+            // 
+            // pb7
+            // 
+            this.pb7.Location = new System.Drawing.Point(246, 417);
+            this.pb7.Name = "pb7";
+            this.pb7.Size = new System.Drawing.Size(135, 131);
+            this.pb7.TabIndex = 6;
+            this.pb7.TabStop = false;
+            this.pb7.Click += new System.EventHandler(this.pb1_Click);
+            // 
+            // btStart
+            // 
+            this.btStart.Location = new System.Drawing.Point(246, 578);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(440, 100);
+            this.btStart.TabIndex = 9;
+            this.btStart.Text = "Start/Nytt spill";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1081, 755);
+            this.Controls.Add(this.btStart);
+            this.Controls.Add(this.pb9);
+            this.Controls.Add(this.pb8);
+            this.Controls.Add(this.pb7);
+            this.Controls.Add(this.pb6);
+            this.Controls.Add(this.pb5);
+            this.Controls.Add(this.pb4);
+            this.Controls.Add(this.pb3);
+            this.Controls.Add(this.pb2);
+            this.Controls.Add(this.pb1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb7)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pb1;
+        private System.Windows.Forms.PictureBox pb2;
+        private System.Windows.Forms.PictureBox pb3;
+        private System.Windows.Forms.PictureBox pb6;
+        private System.Windows.Forms.PictureBox pb5;
+        private System.Windows.Forms.PictureBox pb4;
+        private System.Windows.Forms.PictureBox pb9;
+        private System.Windows.Forms.PictureBox pb8;
+        private System.Windows.Forms.PictureBox pb7;
+        private System.Windows.Forms.Button btStart;
+    }
+}
+
---
+++ Kul_læringsapp/Kul_læringsapp/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kul_læringsapp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[,] bilder = new string[10, 2];
+        PictureBox[] pic = new PictureBox[9];
+        Timer t = new Timer();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bilder[0, 0] = "Hund.jpg";
+            bilder[0, 1] = "Hundtekst";
+            pb1.Image = Image.FromFile("Løve.jpg");
+            pb1.Enabled = false;
+        }
+
+        /// <summary>
+        /// Hente inn 4 tilfeldige bilder og de skriftbildene som hører til
+        /// og gi de tilfeldige referanseverdier slik at de enkelt kan legges inn i picturbox'ene.
+        /// Hente inn ett bilde og gi det den siste plassen.
+        /// Sette 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void pb1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
